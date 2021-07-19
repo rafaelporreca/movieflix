3982f63727b9368d6cb349e8352b06dd5aee592c
Rafael Porreca <rafael.porreca@gmail.com>
2021-07-19 17:36:32 -0300

    New user constructor

--- backend/src/main/java/br/com/rafaelporreca/movieflix/entities/User.java
+++ backend/src/main/java/br/com/rafaelporreca/movieflix/entities/User.java
@@ -35,6 +35,10 @@ public class User implements Serializable, UserDetails {
     public User() {
     }
 
+    public User(Long id) {
+        this.id = id;
+    }
+
     public User(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
